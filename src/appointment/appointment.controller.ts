99db6061654721ba71a3c4907f3f84203ec043b4
payam <payam.shr@yahoo.com>
2025-06-05 13:28:20 +0200

    pure func in contoller created

--- src/appointment/appointment.controller.ts
+++ src/appointment/appointment.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from "@nestjs/common";
 
-@Controller('appointment')
-export class AppointmentController {}
+@Controller("appointment")
+export class AppointmentController {
+
+  @Post()  
+  createApppoinment() {}
+
+  @Get()
+  getAllAppoinment() {}
+
+  @Get('date')
+  getAppoinmentByDate() {}
+
+  @Get(':id')
+  getAppontmentById() {}
+
+  @Put('id')
+  updateApppoinmentById() {}
+
+  @Put('date')
+  updateApppoinmentByDate() {}
+
+  @Delete(":id")
+  deleteApppoinmentById() {}
+
+  @Delete('date')
+  deleteApppoinmentByDate() {}
+}
