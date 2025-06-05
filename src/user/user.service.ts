671d95ce0caf2e213b6d0cffb7c505a6cbb96d23
payam <payam.shr@yahoo.com>
2025-06-05 10:18:32 +0200

    created user with find exist it

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,20 +6,22 @@ import { PrismaService } from "prisma/prisma.service";
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
   async create(dataRq: CreateUserDto) {
-            const user =this.prisma.user.create({
-              data:{
-                  name:dataRq.name,
-                  family:dataRq.family,
-                  phone:dataRq.phone,
-                  password:dataRq.password,
-                  sex:dataRq.sex,
-                  role:dataRq.role,
-                  is_verified:dataRq.is_verified,
-                  email:dataRq.email
-
-                
-              }
-            })    
-    return 'user';
+    const isUserExist = await this.prisma.user.findFirst({
+      where: { OR: [{ email: dataRq.email }, { phone: dataRq.phone }] },
+    });
+    if (isUserExist) return "user already exist"; 
+    const user = await this.prisma.user.create({
+      data: {
+        name: dataRq.name,
+        family: dataRq.family,
+        phone: dataRq.phone,
+        password: dataRq.password,
+        sex: dataRq.sex,
+        role: dataRq.role,
+        is_verified: dataRq.is_verified,
+        email: dataRq.email,
+      },
+    });
+    return user;
   }
 }
