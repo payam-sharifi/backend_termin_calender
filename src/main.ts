6cb810e93f012c4d5531752e3b42135f128d7cf9
payam <payam.shr@yahoo.com>
2025-06-05 17:20:13 +0200

    swaager config

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
+
+  const config = new DocumentBuilder()
+    .setTitle('Appointment App')
+    .setDescription('Api for Appointment')
+    .setVersion('1.0')
+    .addTag('Appointemnt')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
