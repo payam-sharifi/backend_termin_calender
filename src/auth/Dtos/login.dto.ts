cbdacab58758eacb11346ed2e083775d13687b71
payam <payam.shr@yahoo.com>
2025-06-22 19:46:35 +0200

    fake merge

--- src/auth/Dtos/login.dto.ts
+++ src/auth/Dtos/login.dto.ts
@@ -12,6 +12,7 @@ export class LoginUserDto {
   @IsPhoneNumber("DE", { message: "Die Telefonnummer muss eine gültige deutsche Nummer sein." })
   phone: string;
 
+
   @ApiProperty({ example: "password123" })
   @IsString({ message: "Das Passwort muss eine Zeichenkette sein." })
   @Length(5, 12, { message: "Das Passwort muss zwischen 5 und 12 Zeichen lang sein." })
